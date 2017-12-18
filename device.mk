96e2c899d226c175f98fbe2202bf7eeb4f910e24
mj1687 <alessandro.pasini12@gmail.com>
2017-12-18 12:34:12 +0100

    dumpling: remove keylayout and enable navbar
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

--- device.mk
+++ device.mk
@@ -248,11 +248,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/fpc1020.kl:system/usr/keylayout/fpc1020.kl \
-    $(LOCAL_PATH)/keylayout/synaptics.kl:system/usr/keylayout/synaptics.kl
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
